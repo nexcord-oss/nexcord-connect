bfbc0907ed55f29cd72ad54c86726d224d25c96d
Aoun Abu Hassan <aounabuhassan@gmail.com>
2026-05-29 19:43:04 +0300

    fix: use webpack from Next.js context instead of direct import

--- apps/demo/next.config.ts
+++ apps/demo/next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from 'next';
-import webpack from 'webpack';
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   transpilePackages: ['@nexcord-oss/connect'],
-  webpack: (config) => {
+  webpack: (config, { webpack }) => {
     config.plugins.push(
       new webpack.IgnorePlugin({
         resourceRegExp: /^@react-native-async-storage\/async-storage$/,
